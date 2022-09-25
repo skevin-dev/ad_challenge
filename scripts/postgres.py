beb1a192264cdfff7cfc8580247bfcb979d9061e
Kevin <shyakakevin1@gmail.com>
2022-09-25 14:40:47 +0200

    finalizing postgres class

--- scripts/postgres.py
+++ scripts/postgres.py
@@ -1,3 +1,4 @@
+from symbol import return_stmt
 import psycopg2
 import pandas as pd 
 
@@ -10,9 +11,7 @@ class PostgresDBUtils:
     -------
 
     1. create_db: aims to create database
-    2. create_table: aims to create a table in database
-    3. insert_into_table: aims to insert data into a table created
-    4. execute_fetch_db: to fetch data from a table in database
+    2. execute_fetch_db: to fetch data from a table in database
 
     """
 
@@ -53,15 +52,66 @@ class PostgresDBUtils:
 
     def create_db(self, db_name: str) -> None:
         """Creates a database with the specified database name.
-        
+
         Parameters
         ----------
         db_name : str
             The name of the database we are creating
-        """
 
+        Returns
+        -------
+
+        None
+
+        """
         # Preparing query to create a database
         sql = f'''CREATE database ${db_name}'''
         # Creating a database
         self.cursor.execute(sql)
+
+    def db_execute_fetch(self, table_name='', return_df=True) -> pd.DataFrame:
+        """Fetches dataframe from a table with the specified name.
+
+        Parameters
+        ----------
+        table_name : str
+            The name of the table we are inserting into
+        return_df: bool
+            Return dataframe or list
+
+        Returns
+        -------
+
+        Result: a dataframe 
+
+        """
+
+        query = f""" select * from {table_name}"""
+        
+        self.cursor.execute(query)
+        # get column names
+        field_names = [col[0] for col in self.cursor.description]
+
+        # get column values
+        result = self.cursor.fetchall()
+
+        # get row count and show info
+        number_of_rows = self.cursor.rowcount
+        if table_name:
+            print(f"{number_of_rows} records fetched from {table_name} table")
+
+        self.cursor.close()
+        self.conn.close()
+
+        if return_df:
+            return pd.DataFrame(result, columns=field_names)
+        else:
+            return result
+
+    def close_connection(self):
+        """
+        Close the connection with the database.
+        """
+        self.conn.close()
+
 
